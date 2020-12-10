5b25e08bb938881fd5cc6ebbcc27dce02759b1ed
Denny Hong <denny@beacondigitalmarketing.com>
2020-12-09 20:14:11 -0800

    Adds jumbotron compound component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Hello</div>;
-}
+import Jumbotron from "./components/jumbotron";
+import jumboData from "./fixtures/jumbo.json";
+
+const App = () => {
+  return jumboData.map((item) => (
+    <Jumbotron.Container key={item.id} direction={item.direction}>
+      <Jumbotron direction={item.direction}>
+        <Jumbotron.Pane>
+          <Jumbotron.Title>{item.title}</Jumbotron.Title>
+          <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+        </Jumbotron.Pane>
+
+        <Jumbotron.Pane>
+          <Jumbotron.Image src={item.image} alt={item.alt} />
+        </Jumbotron.Pane>
+      </Jumbotron>
+    </Jumbotron.Container>
+  ));
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { render } from "react-dom";
 import "normalize.css";
 
 import App from "./App";
+import { GlobalStyles } from "./globalStyles";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Fragment>
+    <GlobalStyles />
+    <App />
+  </Fragment>,
+  document.getElementById("root")
+);
